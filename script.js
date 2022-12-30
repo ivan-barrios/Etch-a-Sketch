73c64f81a7307a93417ab8b666e375a680cf6ec2
Ivan <ivanbar8112003@gmail.com>
2022-12-30 19:34:11 -0300

    Board done

---
+++ script.js
@@ -0,0 +1,64 @@
+let color = "black";
+let click = false;
+
+
+
+//CREATES GRID
+const container = document.getElementById('container');
+function  makeGrid(value){
+    let items = document.querySelectorAll('.grid-item');
+    items.forEach((div) => div.remove());
+    container.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${value}, 1fr)`;
+    let amount = value * value;
+    for (let i = 0; i < (amount); i++){
+        let item = document.createElement('div');
+        item.addEventListener('mouseover', colorGridItem)
+        item.style.backgroundColor = "white"
+        container.appendChild(item).className = "grid-item";
+    }
+}
+makeGrid(16);
+
+
+//Changes the size of the board
+function changeSize(value){
+    if (value >= 2 && value <= 100){
+        makeGrid(value);
+    }
+}
+
+
+//Changes the color of the pen
+function colorGridItem(item){
+    if (click){
+        this.style.backgroundColor = color;
+    }
+}
+function changeColor(choice){
+    color = choice;
+}
+
+
+//If clicks, the pen works, if unclick, it does not
+document.querySelector('#container').addEventListener('mousedown', () => {
+    click = !click;
+})
+
+
+
+function clearBoard(){
+    let board = document.querySelectorAll('.grid-item');
+    board.forEach((board) => {
+        board.style.backgroundColor = "white";
+    })
+}
+
+
+
+//To get all buttons with their respective color
+const colorBtns = document.querySelectorAll('.colorButton');
+colorBtns.forEach((btn) => {
+    btn.style.backgroundColor = btn.textContent;
+    btn.style.color = btn.textContent;
+})
